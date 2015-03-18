64fc17b425ebfcea9dbc1507b10c20780f214132
Tyguy160 <Tylermaschino@gmail.com>
2015-03-18 13:58:48 -0400

    Initial commit.

---
+++ OS_C/Tutorial3/scripts/CMakeFiles/armc-011.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_ASM
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/armc-011-start.S" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/scripts/CMakeFiles/armc-011.dir/armc-011-start.S.obj"
+  )
+SET(CMAKE_ASM_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/armc-011-cstartup.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/scripts/CMakeFiles/armc-011.dir/armc-011-cstartup.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/armc-011-cstubs.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/scripts/CMakeFiles/armc-011.dir/armc-011-cstubs.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/armc-011.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/scripts/CMakeFiles/armc-011.dir/armc-011.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/rpi-systimer.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial3/scripts/CMakeFiles/armc-011.dir/rpi-systimer.c.obj"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_ASM
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/armc-013-start.S" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/armc-013-start.S.obj"
+  )
+SET(CMAKE_ASM_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/armc-013-cstartup.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/armc-013-cstartup.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/armc-013-cstubs.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/armc-013-cstubs.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/armc-013.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/armc-013.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/rpi-armtimer.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/rpi-armtimer.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/rpi-gpio.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/rpi-gpio.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/rpi-interrupts.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/rpi-interrupts.c.obj"
+  "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/rpi-systimer.c" "/home/tyler/RPi1/DCS-RPi/OS_C/Tutorial4/scripts/CMakeFiles/armc-013.dir/rpi-systimer.c.obj"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
